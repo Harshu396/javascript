fcec0616ebd87eb39ee7e4a952a51b431b355173
Harshu396 <harshugangawane3@gmail.com>
2025-01-17 10:32:23 +0000

    datatype and variable

---
+++ basic/datatypes.js
@@ -0,0 +1,14 @@
+"use strict" //all js code new code versions
+
+alert // using nodejs
+
+//data types in js
+
+
+//Number
+//String ""
+//Boolean true or false
+//null
+//undefined
+
+
---
+++ basic/variable.js
@@ -0,0 +1,8 @@
+const accountid=123;
+let name ="harshsa";
+var emailid="abc@gmail.com";
+
+
+console.log(accountid) 
+
+console.table([accountid, name , emailid]);
